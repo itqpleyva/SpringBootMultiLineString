32337779fe2940c1a3df5e1bfdaf9b1e96c50b4a
itqpleyva@gmail.com <itqpleyva@gmail.com>
2020-06-28 12:15:24 -0400

    first commit

---
+++ src/main/java/Main/Main.java
@@ -0,0 +1,39 @@
+package Main;
+
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Main implements CommandLineRunner {
+
+	public static void main(String[] args) {
+		
+		SpringApplication.run(Main.class, args);
+		System.out.println("I am in");
+
+	}
+
+	public void run(String... args) throws Exception {
+		
+		String reporte = "";
+		
+		for (int i = 0; i < 10; i++) {
+			
+			reporte = reporte+"Line"+i+ "\n";
+		}
+		System.out.println(reporte);
+		
+  	    FileWriter fileWriter = new FileWriter("file.txt", true);
+  	    BufferedWriter bufferedWriter = new BufferedWriter(fileWriter);
+  	    bufferedWriter.write(reporte + "\n");
+  	    bufferedWriter.flush();
+  	    bufferedWriter.close();
+  	    fileWriter.close();
+		
+	}
+
+}
